f756eb4283a8f434b54245c53c457067a2efbf31
Martin <martin@awadlouis.com>
2026-02-18 16:40:26 +0100

    test: add integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,5 +1,6 @@
 from src.DSU import DSU
 from src.QStack import QStack
+from src.squeue import SQueue
 
 def test_QStack_DSU():
     stack:QStack = QStack()    
@@ -17,3 +18,20 @@ def test_QStack_DSU():
     assert stack.pop() == dsu4
 
     assert stack.peek() == dsu3
+
+def test_squeue_DSU():
+    squeue:SQueue = SQueue()
+
+    dsu1:DSU = DSU(2)
+    dsu2:DSU = DSU(3)
+    dsu3:DSU = DSU(4)
+    dsu4:DSU = DSU(5)
+
+    squeue.push(dsu1)
+    squeue.push(dsu2)
+    squeue.push(dsu3)
+    squeue.push(dsu4)
+
+    assert squeue.pop() == dsu1
+
+    assert squeue.peek() == dsu2
